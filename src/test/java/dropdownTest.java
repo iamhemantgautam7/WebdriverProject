dcfecefc4b3b3fe754ea6d4f8402bfbccce63047
iamhemantgautam7 <iamhemantgautam7@gmail.com>
2024-04-23 06:03:50 +0000

    drop_down code

---
+++ src/test/java/dropdownTest.java
@@ -0,0 +1,40 @@
+import java.util.List;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.chrome.ChromeOptions;
+import org.openqa.selenium.support.ui.Select;
+
+public class dropdownTest {
+
+	public static void main(String[] args) {
+
+
+		ChromeOptions options = new ChromeOptions();
+		options.setBrowserVersion("115");
+//		options.addArguments("--headless");  
+		
+		WebDriver driver = new ChromeDriver(options);
+		driver.manage().window().maximize();
+		driver.get("https://demo.seleniumeasy.com/basic-select-dropdown-demo.html");
+		
+		Select singSel=new Select(driver.findElement(By.id("select-demo")));
+		singSel.selectByValue("Tuesday");
+		
+		Select mulSel =new Select(driver.findElement(By.id("multi-select")));
+		if(mulSel.isMultiple()) {
+			mulSel.selectByIndex(0);
+			mulSel.selectByValue("Florida");
+			mulSel.selectByVisibleText("Washington");
+			
+			}
+		
+		List <WebElement> items=mulSel.getAllSelectedOptions();
+		System.out.println("Items Seleccted ="+items.size());
+		mulSel.deselectByValue("Florida");
+
+	}
+
+}
